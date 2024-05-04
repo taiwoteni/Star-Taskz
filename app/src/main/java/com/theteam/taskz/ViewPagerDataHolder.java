3c9ae4b6496590ff747acc016a76e03833692db4
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-04 18:12:06 +0100

    Initial Commit on Tue, March 12

---
+++ app/src/main/java/com/theteam/taskz/ViewPagerDataHolder.java
@@ -0,0 +1,9 @@
+package com.theteam.taskz;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class ViewPagerDataHolder {
+
+    static ViewPager2 viewPager;
+}
