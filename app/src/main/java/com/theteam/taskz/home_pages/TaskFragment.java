6758524c644b49a5f7c462041e2a15a794776d1d
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-05 10:25:47 +0100

    Added the fragments used in home

---
+++ app/src/main/java/com/theteam/taskz/home_pages/TaskFragment.java
@@ -0,0 +1,42 @@
+package com.theteam.taskz.home_pages;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.theteam.taskz.R;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class TaskFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tasks_fragment, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+    void showMessage(final String message){
+        Toast.makeText(requireActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
