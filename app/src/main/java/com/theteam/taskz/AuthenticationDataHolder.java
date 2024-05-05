0d892a311c5112c96eb915c2a17a442c32e49bc2
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-05 08:56:19 +0100

    Data holder for login data

--- app/src/main/java/com/theteam/taskz/AuthenticationDataHolder.java
+++ app/src/main/java/com/theteam/taskz/AuthenticationDataHolder.java
@@ -1,12 +1,13 @@
 package com.theteam.taskz;
 
 public class AuthenticationDataHolder {
-    static String firstName,lastName,email,password;
+    static String firstName,lastName,email,password, dob;
 
     static void clear(){
         firstName = null;
         lastName = null;
         email = null;
         password = null;
+        dob = null;
     }
 }
