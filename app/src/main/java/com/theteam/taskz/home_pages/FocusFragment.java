90d925785a03940f6caf26ab2f6aebaeb6163ec9
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-07 13:12:14 +0100

    ...

---
+++ app/src/main/java/com/theteam/taskz/home_pages/FocusFragment.java
@@ -0,0 +1,38 @@
+package com.theteam.taskz.home_pages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.theteam.taskz.R;
+
+public class FocusFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.focus_fragment, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+    void showMessage(final String message){
+        Toast.makeText(requireActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
