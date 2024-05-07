c63babc9710a1811ec101960caafab0f2da8ab6a
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-07 13:09:11 +0100

    ...

---
+++ app/src/main/java/com/theteam/taskz/CalendarPageAdapter.java
@@ -0,0 +1,39 @@
+package com.theteam.taskz;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class CalendarPageAdapter extends FragmentStateAdapter {
+
+    private ArrayList<Fragment> views = new ArrayList<>();
+
+    public CalendarPageAdapter(Fragment fragment, ArrayList<Calendar> dates) {
+        super(fragment);
+        ArrayList<Fragment> views = new ArrayList<>();
+        for(final Calendar calendar: dates){
+            views.add(new CalendarPageFragment(calendar));
+        }
+        
+        this.views = views;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        Log.d("FRAGMENT", "Opened");
+        return views.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        // Return the total number of fragments
+        return views.size(); // Adjust as needed based on the number of fragments you have
+    }
+}
+
