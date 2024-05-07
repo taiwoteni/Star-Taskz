d751301e9b6f1b0e4c3414f3f2545a715ddbf612
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-07 13:05:10 +0100

    ...

--- app/src/main/java/com/theteam/taskz/MainActivity.java
+++ app/src/main/java/com/theteam/taskz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.theteam.taskz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -24,12 +27,35 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean startedAnimation = false;
 
+    private static final int REQUEST_NOTIFICATION_PERMISSION = 1;
 
+    // Call this method from your activity or fragment
+    private void requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS, Manifest.permission.SYSTEM_ALERT_WINDOW}, REQUEST_NOTIFICATION_PERMISSION);
+            }
+        }
+    }
+
+    // Override onRequestPermissionsResult to handle permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can post notifications
+            } else {
+                // Permission denied, handle accordingly
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestNotificationPermission();
 
         app_name = (TextView) findViewById(R.id.app_name);
         lottie = (LottieAnimationView) findViewById(R.id.loading_lottie);
