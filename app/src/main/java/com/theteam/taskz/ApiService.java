3cb9955a2b08dbab73fec2de82627167ee1ae6e7
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-08 20:05:46 +0100

    Finished Tasks , Calendar and Pomodorro

---
+++ app/src/main/java/com/theteam/taskz/ApiService.java
@@ -0,0 +1,16 @@
+package com.theteam.taskz;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+public class ApiService {
+    public static String openWeatherKey = "9e77dbfd2cc45955ad36269ba273fa69";
+
+
+}
