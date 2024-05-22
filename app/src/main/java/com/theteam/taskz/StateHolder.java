0086e95009851f666389fd88391f25a334afcf71
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-22 15:44:25 +0100

    Edited..

---
+++ app/src/main/java/com/theteam/taskz/StateHolder.java
@@ -0,0 +1,8 @@
+package com.theteam.taskz;
+
+import java.util.List;
+
+public class StateHolder {
+    public static List<TasksPageFragment> taskPageFragments;
+    public static List<CalendarPageFragment> calendarPageFragments;
+}
