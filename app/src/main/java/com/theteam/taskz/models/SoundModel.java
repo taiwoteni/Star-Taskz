6d976bcd8682fa0deb045bf34be2d3228f21ccbf
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-22 16:39:38 +0100

    Created Sound Model

---
+++ app/src/main/java/com/theteam/taskz/models/SoundModel.java
@@ -0,0 +1,11 @@
+package com.theteam.taskz.models;
+
+public class SoundModel {
+    public String name;
+    public String url;
+
+    public SoundModel(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+}
