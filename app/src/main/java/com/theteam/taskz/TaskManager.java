f6cad1803d858750c70f5b463cb727d403b6b410
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-22 15:52:39 +0100

    Adjustments...

--- app/src/main/java/com/theteam/taskz/TaskManager.java
+++ app/src/main/java/com/theteam/taskz/TaskManager.java
@@ -2,12 +2,20 @@ package com.theteam.taskz;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +24,13 @@ import java.util.Objects;
 public class TaskManager {
     final private ArrayList<TaskModel> tasks = new ArrayList<>();
 
+    private RequestQueue requestQueue;
+
+    public static String END_POINT = "https://startaskzbackend-production.up.railway.app/";
+
     private Context context;
+
+    private UserModel userModel;
     public TaskManager(Context context){
         this.context = context;
         SharedPreferences preferences = context.getSharedPreferences("GLOBAL", Context.MODE_PRIVATE);
@@ -32,8 +46,11 @@ public class TaskManager {
             tasks.add(new TaskModel(json));
         }
 
+        requestQueue = Volley.newRequestQueue(context);
+
     }
     public void addTask(TaskModel model){
+        userModel = new UserModel(context);
         tasks.add(model);
         Gson gson = new Gson();
         SharedPreferences preferences = context.getSharedPreferences("GLOBAL", Context.MODE_PRIVATE);
@@ -42,6 +59,35 @@ public class TaskManager {
         ArrayList<HashMap<String, Object>> list = gson.fromJson(jsonString, listType);
         list.add(model.toJson());
         preferences.edit().putString("tasks", gson.toJson(list, new TypeToken<ArrayList<HashMap<String,Object>>>(){}.getType())).apply();
+
+        refresh(model);
+
+
+//        StringRequest createTaskRequest = new StringRequest(
+//                Request.Method.POST,
+//                END_POINT + "user/create-task/" + userModel.uid(),
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String string) {
+//                        Log.v("API_REP", string);
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError volleyError) {
+//                        Log.e("API_REP", volleyError.getMessage());
+//                    }
+//                }
+//                ) {
+//            @Override
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+//                // Add more parameters as needed
+//                return params;
+//            }
+//        };
     }
 
     public void deleteTask(TaskModel model){
@@ -63,6 +109,8 @@ public class TaskManager {
             list.remove(index);
         }
         preferences.edit().putString("tasks", gson.toJson(list, new TypeToken<ArrayList<HashMap<String,Object>>>(){}.getType())).apply();
+
+        refresh(model);
     }
 
     public void updateTask(TaskModel model){
@@ -90,14 +138,49 @@ public class TaskManager {
             }
         }
         preferences.edit().putString("tasks", gson.toJson(list)).apply();
+
+        refresh(model);
     }
 
-
     public ArrayList<TaskModel> getTasks(){
+        SharedPreferences preferences = context.getSharedPreferences("GLOBAL", Context.MODE_PRIVATE);
+        final String jsonString = preferences.getString("tasks", "[]");
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<HashMap<String, Object>>>(){}.getType();
+        ArrayList<HashMap<String, Object>> list = gson.fromJson(jsonString, listType);
+        tasks.clear();
+        for(final HashMap<String,Object> json : list){
+            if(!json.containsKey("id")){
+                json.put("id", "#TASK-" + list.indexOf(json));
+            }
+            tasks.add(new TaskModel(json));
+        }
         return tasks;
     }
 
-    public static TasksListAdapter tasksListAdapter;
+    private void refresh(TaskModel model){
+        if(StateHolder.taskPageFragments!= null){
+            //Inorder to automatically refresh the Page that needs this update,
+            for(TasksPageFragment fragment: StateHolder.taskPageFragments){
+                if(fragment.date.get(Calendar.MONDAY) == model.date.get(Calendar.MONTH) && fragment.date.get(Calendar.DAY_OF_MONTH) == model.date.get(Calendar.DAY_OF_MONTH)){
+                    //We refresh
+                    if(fragment.requireActivity() != null){
+                        fragment.instantiateTasks();
+                    }
+                }
+            }
+        }
+        if(StateHolder.calendarPageFragments != null){
+            for(CalendarPageFragment fragment: StateHolder.calendarPageFragments){
+                if(fragment.date.get(Calendar.MONDAY) == model.date.get(Calendar.MONTH) && fragment.date.get(Calendar.DAY_OF_MONTH) == model.date.get(Calendar.DAY_OF_MONTH)){
+                    //We refresh
+                    if(fragment.requireActivity() != null){
+                        fragment.instantiateTasks();
+                    }
+                }
+            }
 
-    public static CalendarListAdapter calendarListAdapter;
+        }
+    }
+
 }
