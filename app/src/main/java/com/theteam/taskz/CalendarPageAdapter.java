5431eca784767db6b85cfbbd1e1ca29727626a77
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-22 15:51:38 +0100

    Adjustments...

--- app/src/main/java/com/theteam/taskz/CalendarPageAdapter.java
+++ app/src/main/java/com/theteam/taskz/CalendarPageAdapter.java
@@ -16,8 +16,15 @@ public class CalendarPageAdapter extends FragmentStateAdapter {
     public CalendarPageAdapter(Fragment fragment, ArrayList<Calendar> dates) {
         super(fragment);
         ArrayList<Fragment> views = new ArrayList<>();
+        //We first initialize the List of CalendarPageFragments
+        StateHolder.calendarPageFragments = new ArrayList<>();
         for(final Calendar calendar: dates){
-            views.add(new CalendarPageFragment(calendar));
+            //Then we declare an instantiated CalendarPageFragment, inorder to add the same Object to both the views array and
+            // CalendarPageFragments
+            final CalendarPageFragment calendarPageFragment = new CalendarPageFragment(calendar);
+            views.add(calendarPageFragment);
+            StateHolder.calendarPageFragments.add(calendarPageFragment);
+
         }
         
         this.views = views;
