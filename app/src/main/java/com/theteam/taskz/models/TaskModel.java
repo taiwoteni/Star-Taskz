d08585d6f67bc1faf6e71e03f5727996daf5d969
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-24 20:20:53 +0100

    Successfully consumed all APIs and made other adjustments concerning Alarm and Notification functionalities

--- app/src/main/java/com/theteam/taskz/models/TaskModel.java
+++ app/src/main/java/com/theteam/taskz/models/TaskModel.java
@@ -1,10 +1,13 @@
 package com.theteam.taskz.models;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.theteam.taskz.enums.TaskStatus;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,6 +20,7 @@ public class TaskModel {
     public Calendar date;
     public String name;
     public String id;
+    public String globalId;
 
     public int notifId = 0;
     public boolean notifIdExists = false;
@@ -25,13 +29,24 @@ public class TaskModel {
     public TaskStatus status;
 
 
-
+    private TaskModel(){}
     public TaskModel(Map<String, Object> json){
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(parseStringToTime(json.get("time").toString()));
 
         date = calendar;
         id = json.get("id").toString();
+        //The global id can be the same as the id
+        //So when parsing, it may give a NumberFormatException because it'll be like : '#TASK-?'
+        //So this will help us determine wether we should parse it or
+        //... Leave it(Derived from 'id')
+        try {
+            globalId = String.valueOf((int) Double.parseDouble(json.get("globalId").toString()));
+        }
+        catch (NumberFormatException e){
+            globalId = json.get("globalId").toString();
+
+        }
         name = json.get("name").toString();
         category = json.get("category").toString();
         if(json.containsKey("notifId")){
@@ -52,6 +67,45 @@ public class TaskModel {
         }
 
     }
+
+    //We create a factory constructor to decode json data given in different key-values from API
+    public static TaskModel fromJsonObject(JSONObject object){
+        final Type type = new TypeToken<HashMap<String,Object>>(){}.getType();
+        final HashMap<String,Object> json = new HashMap<>();
+        final Gson gson = new Gson();
+        final HashMap<String,Object> objectMap = gson.fromJson(object.toString(), type);
+
+        json.put("name", objectMap.get("taskName").toString());
+        json.put("id", objectMap.get("fakeId").toString());
+        json.put("globalId", objectMap.get("id").toString());
+        json.put("status", objectMap.get("taskStatus")==null? "pending":objectMap.get("taskStatus").toString().toLowerCase());
+        json.put("category", objectMap.get("taskCategory")==null? "Uncategorized":objectMap.get("taskCategory").toString().toLowerCase());
+
+
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        final SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
+
+        Calendar calendar = Calendar.getInstance();
+
+        try {
+            calendar.setTimeInMillis(dateFormat.parse(objectMap.get("startDate").toString()).getTime());
+            Calendar timeCalendar = Calendar.getInstance();
+            timeCalendar.setTime(timeFormat.parse(objectMap.get("startTime").toString()));
+            calendar.set(Calendar.HOUR_OF_DAY, timeCalendar.get(Calendar.HOUR_OF_DAY));
+            calendar.set(Calendar.MINUTE, timeCalendar.get(Calendar.MINUTE));
+            calendar.set(Calendar.SECOND, timeCalendar.get(Calendar.SECOND));
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        json.put("time", calendar.getTimeInMillis());
+
+
+        return new TaskModel(json);
+
+    }
+
     private static long parseStringToTime(String timeString) {
         try {
             // Try parsing without handling scientific notation
@@ -82,6 +136,9 @@ public class TaskModel {
         if(notifIdExists){
             json.put("notifId", String.valueOf(notifId));
         }
+        // If the globalId is the same as the id.
+        // Then it means the globalId the globalId(Task id from API) does not exists.
+        json.put("globalId", globalId);
         json.put("time", date.getTimeInMillis());
         json.put("status", status.name());
 
@@ -95,13 +152,32 @@ public class TaskModel {
         final SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
 
 
+        // Making a different calendar and setting the time
+        // To a day more than the startDate and startTime.
+        // Just to check if API error is due to the times being the same.
+
+        Calendar endDate = (Calendar) this.date.clone();
+        final int dayOfYear = date.get(Calendar.DAY_OF_YEAR);
+        final int year = date.get(Calendar.YEAR);
+        boolean isLeapYear = date.get(Calendar.YEAR)%4 ==0;
+        boolean isLastDay = isLeapYear? dayOfYear==365:dayOfYear==366;
+        endDate.set(Calendar.DAY_OF_YEAR, isLastDay? 1: date.get(Calendar.DAY_OF_YEAR)+1);
+        endDate.set(Calendar.YEAR, isLastDay? year+1:year);
+
         JSONObject jsonObject = new JSONObject();
+
+        // I.e if the task has id gotten from API
+        // The globalId and id cannot be the same because id from offline has '#TASK-' while the id from API is number.
+        if(!globalId.equals(id)){
+            jsonObject.put("id", Integer.parseInt(globalId));
+        }
+
         jsonObject.put("taskName", name);
         jsonObject.put("taskDescription", name);
-        jsonObject.put("startDate", dateFormat.format(date));
-        jsonObject.put("startTime", isoFormat.format(date));
-        jsonObject.put("endDate", dateFormat.format(date));
-        jsonObject.put("endTime", isoFormat.format(date));
+        jsonObject.put("startDate", dateFormat.format(date.getTime()));
+        jsonObject.put("startTime", isoFormat.format(date.getTime()));
+        jsonObject.put("endDate", dateFormat.format(endDate.getTime()));
+        jsonObject.put("endTime", isoFormat.format(endDate.getTime()));
         jsonObject.put("taskStatus",status.name().toUpperCase());
         jsonObject.put("taskCategory", category.toUpperCase());
 
