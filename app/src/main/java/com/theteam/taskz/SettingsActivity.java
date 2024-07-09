0d18cf8a981fbee2b79afb5c4e64e2314d9d0a7f
Ifechukwu Nwachukwu <88788858+ICN-1@users.noreply.github.com>
2024-07-09 12:25:23 +0100

    Settings Page ICN (#3)
    Co-authored-by: Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>

---
+++ app/src/main/java/com/theteam/taskz/SettingsActivity.java
@@ -0,0 +1,74 @@
+package com.theteam.taskz;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+    boolean isDarkChecked, isSystemChecked;
+    int systemTheme;
+    SwitchCompat dark_mode_switch;
+    SharedPreferences themePreferences;
+    SharedPreferences.Editor editThemePreference;
+    UiModeManager uiModeManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        dark_mode_switch = findViewById(R.id.dark_mode_switch);
+
+        uiModeManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
+        systemTheme = uiModeManager.getNightMode();
+        if (systemTheme == UiModeManager.MODE_NIGHT_NO){
+            isSystemChecked = false;
+        }else if (systemTheme == UiModeManager.MODE_NIGHT_YES){
+            isSystemChecked = true;
+        }
+
+        themePreferences = getSharedPreferences("App_Theme", MODE_PRIVATE);
+        isDarkChecked = themePreferences.getBoolean("isDark", isSystemChecked);
+        editThemePreference = themePreferences.edit();
+        isDarkMode(isDarkChecked);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        dark_mode_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isDarkMode(isChecked);
+                editThemePreference.putBoolean("isDark", isChecked).commit();
+            }
+        });
+    }
+
+    private void isDarkMode(boolean isChecked){
+        if (isChecked){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            dark_mode_switch.setChecked(true);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            dark_mode_switch.setChecked(false);
+        }
+    }
+}
