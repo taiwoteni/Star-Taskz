bd28b333eb5ff35c7004c6e5ec4047729f23cf6c
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-07-23 04:58:46 +0100

    Lot of things

--- app/src/main/java/com/theteam/taskz/domain/repositories/Jira.java
+++ app/src/main/java/com/theteam/taskz/domain/repositories/Jira.java
@@ -1,8 +1,8 @@
 package com.theteam.taskz.domain.repositories;
 
 public class Jira {
-    public static String jiraString = "ATOAX2q8DgUENOHTyNRVWmoInbldYNTEBfOBl_HzqU-Dp-xjdA6NQgSRFvUesBzMo0Rx23242F13";
-    public static String jiraInt = "aG2HygzaXKij4bKXAdcQppsUVSjzWEuO"; //li
+    public static String jiraSecret = "ATOAX2q8DgUENOHTyNRVWmoInbldYNTEBfOBl_HzqU-Dp-xjdA6NQgSRFvUesBzMo0Rx23242F13";
+    public static String jiraClient = "aG2HygzaXKij4bKXAdcQppsUVSjzWEuO";
 
 
     public void syncAllTasks(){
